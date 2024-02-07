2a5315a03e4a7feec00ccd4bb0640e985c14ec5d
Gavin Power <gavinmaxpower@gmail.com>
2024-02-06 21:44:46 -0330

    sent from my ipod nano

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,9 @@
+export const Navigation = () => {
+  return (
+    <div className="flex flex-col mt-4 lg:my-0 mx-8 lg:mx-auto">
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+    </div>
+  );
+};
--- src/components/Tama/Tama.tsx
+++ src/components/Tama/Tama.tsx
@@ -11,34 +11,32 @@ export const Tama = () => {
   const [isMuted, setIsMuted] = useState(true);
 
   return (
-    <div>
-      <Button
-        onClick={() => {
-          setIsMuted(!isMuted);
-        }}
-      >
-        {isMuted ? "Unmute" : "Mute"}
-      </Button>
-      <Draggable>
-        <div className="tama-container">
-          <div className="stream-container">
-            <ReactPlayer
-              url="https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8"
-              controls={false}
-              playing={true}
-              muted={isMuted}
-              playsinline // Add this if you want to play inline on iOS devices
-            />
-          </div>
-          <div className="buttons-container">
-            <SSEListener endpoint={"/api/events"}>
-              <TamaButton command="A" />
-              <TamaButton command="B" />
-              <TamaButton command="C" />
-            </SSEListener>
-          </div>
+    <Draggable>
+      <div className="tama-container">
+        <Button
+          onClick={() => {
+            setIsMuted(!isMuted);
+          }}
+        >
+          {isMuted ? "Unmute" : "Mute"}
+        </Button>
+        <div className="stream-container">
+          <ReactPlayer
+            //url="https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8"
+            controls={false}
+            playing={true}
+            muted={isMuted}
+            playsinline // Add this if you want to play inline on iOS devices
+          />
         </div>
-      </Draggable>
-    </div>
+        <div className="buttons-container">
+          <SSEListener endpoint={"/api/events"}>
+            <TamaButton command="A" />
+            <TamaButton command="B" />
+            <TamaButton command="C" />
+          </SSEListener>
+        </div>
+      </div>
+    </Draggable>
   );
 };
